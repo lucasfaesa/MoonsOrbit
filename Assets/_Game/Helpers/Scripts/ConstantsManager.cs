d229009bfd1852cc1cbb057a26e1a25a31ffcf47
Lucas Rios <lucasfaesa@gmail.com>
2024-09-09 20:22:33 -0300

    forgot

--- Assets/_Game/Helpers/Scripts/ConstantsManager.cs
+++ Assets/_Game/Helpers/Scripts/ConstantsManager.cs
@@ -5,6 +5,12 @@ namespace Helpers
     public static class ConstantsManager
     {
         public enum TargetType {NONE, HUMAN, MONSTER, METAL}
+
+        public const string PLAYER_LAYER = "Player";
+        public const string NETWORK_PLAYER_LAYER = "NetworkPlayer";
+        public const string ENEMY_LAYER = "Enemy";
+        
+        
     }    
 }
 
